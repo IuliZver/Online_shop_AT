fe4c8de34a1b38212aaf16d64aef4ea380aae7eb
IuliZver <zvereva.iuliia@hotmail.com>
2025-03-21 00:16:05 +0300

    login page, product page, main page checks added

---
+++ pages/locators.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
